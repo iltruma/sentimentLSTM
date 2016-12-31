76a45ada03a1d4a41ac5cf2aefbd0ef0fb4f1a93
Riccardo Grazzi <rikkajounin@gmail.com>
2016-12-31 16:16:30 +0100

    added option for minimum frequency words and improved vocabulary printing

--- Preprocessing/dataprocessor.py
+++ Preprocessing/dataprocessor.py
@@ -11,9 +11,8 @@ from multiprocessing import Process, Lock
 
 dataDir = "../data/"
 dirs = [dataDir + "aclImdb/test/pos", dataDir + "aclImdb/test/neg", \
-        dataDir +"aclImdb/train/pos", dataDir +"aclImdb/train/neg"]
+        dataDir + "aclImdb/train/pos", dataDir + "aclImdb/train/neg"]
 url = "http://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
-
 
 '''
 checks if files and directories for the data are already present, if not makes them
@@ -22,7 +21,9 @@ To speed up the data processing (I probably did it way too inefficiently),
 I decided to split the task in n processes, where n is the number of directories
 Each of the four (test/pos, test/neg, train/pos, train/neg) directory are processed in the same way specified below
 '''
-def run(max_seq_length, max_vocab_size):
+
+
+def run(max_seq_length, max_vocab_size, min_count):
     if not os.path.exists(dataDir):
         print('%s directory not found!' % dataDir)
         return
@@ -40,12 +41,12 @@ def run(max_seq_length, max_vocab_size):
         print("vocab mapping found...")
     else:
         print("no vocab mapping found, running preprocessor...")
-        createVocab(dirs, max_vocab_size)
+        createVocab(dirs, max_vocab_size, min_count)
     if not os.path.exists(dataDir + "processed"):
         os.makedirs(dataDir + "processed/")
         print("No processed data files found, running preprocessor...")
     else:
-        print("Processed data files found: delete " +dataDir+ "processed  to redo them")
+        print("Processed data files found: delete " + dataDir + "processed  to redo them")
         return
     import vocabmapping
     vocab = vocabmapping.VocabMapping()
@@ -61,6 +62,7 @@ def run(max_seq_length, max_vocab_size):
     for p in processes:
         if p.is_alive():
             p.join()
+
 
 '''
 Multithread implementation of the data processing (a thread per directory)
@@ -81,6 +83,8 @@ A lock was used to ensure while writing to std.out bad things don't happen.
       if there are words not present in the vocabulary the index of the '<UNK>' token is used
 - stack all the numpy arrays vertically (as raw of a numpy matrix) and save them to memory
 '''
+
+
 def createProcessedDataFile(vocab_mapping, directory, pid, max_seq_length, lock):
     count = 0
     data = np.array([i for i in range(max_seq_length + 2)])
@@ -94,7 +98,7 @@ def createProcessedDataFile(vocab_mapping, directory, pid, max_seq_length, lock)
             tokens = tokenize(review.read().lower())
             numTokens = len(tokens)
             indices = [vocab_mapping.getIndex(j) for j in tokens]
-            #pad sequence to max length
+            # pad sequence to max length
             if len(indices) < max_seq_length:
                 indices = indices + [vocab_mapping.getIndex("<PAD>") for i in range(max_seq_length - len(indices))]
             else:
@@ -107,7 +111,7 @@ def createProcessedDataFile(vocab_mapping, directory, pid, max_seq_length, lock)
         assert len(indices) == max_seq_length + 2, str(len(indices))
         data = np.vstack((data, indices))
         indices = []
-    #remove first placeholder value
+    # remove first placeholder value
     data = data[1::]
     lock.acquire()
     print("Saving data file{0} to disk...".format(str(pid)))
@@ -115,7 +119,7 @@ def createProcessedDataFile(vocab_mapping, directory, pid, max_seq_length, lock)
     saveData(data, pid)
 
 
-#method from: http://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python
+# method from: http://stackoverflow.com/questions/22676/how-do-i-download-a-file-over-http-using-python
 def downloadFile(url):
     file_name = os.path.join(dataDir, url.split('/')[-1])
     u = urllib.request.urlopen(url)
@@ -140,25 +144,33 @@ def downloadFile(url):
 '''
 This function tokenizes sentences
 '''
+
+
 def tokenize(text):
     return nltk.word_tokenize(text)
+
 
 '''
 Saves processed data numpy array
 '''
+
+
 def saveData(npArray, index):
     name = "data{0}.npy".format(str(index))
     outfile = os.path.join(dataDir + "processed/", name)
     print("numpy array is: {0}x{1}".format(len(npArray), len(npArray[0])))
     np.save(outfile, npArray)
 
+
 '''
 create vocab mapping file and stores it in vocab.txt (this is not a text file but a binary):
 the data is of the form dic = {'the': 0, 'a': 1, 'is' : 2, ..., <UNK>: max_vocab_size, <PAD>: max_vocab_size +1 }
 where the words are ordered from the most frequent to the last.
 '''
-def createVocab(dirs, max_vocab_size):
-    print("Creating vocab mapping...")
+
+
+def createVocab(dirs, max_vocab_size, min_count):
+    print("Creating vocab mapping (max size: %d, min frequency: %d)..." %(max_vocab_size, min_count))
     dic = {}
     for d in dirs:
         indices = []
@@ -173,23 +185,28 @@ def createVocab(dirs, max_vocab_size):
     d = {}
     counter = 0
     for w in sorted(dic, key=dic.get, reverse=True):
+        # take word more frequent than min_count
+        if dic[w] < min_count: break
         d[w] = counter
         counter += 1
-        #take most frequent max_vocab_size tokens
-        if counter >=max_vocab_size:
-            break
-    #add out of vocab token and pad token
+        # take most frequent max_vocab_size tokens
+        if max_vocab_size > -1 and counter >= max_vocab_size: break
+
+    # add out of vocab token and pad token
     d["<UNK>"] = counter
-    counter +=1
+    counter += 1
     d["<PAD>"] = counter
+    print("vocab mapping created: size: %d discarded: %d" % (len(d), len(dic) - len(d) + 2 ))
     with open(dataDir + 'vocab.txt', 'wb') as handle:
         pickle.dump(d, handle)
 
 
 def main():
-    max_seq_length = 200
-    max_vocab_size = 20000
-    run(max_seq_length, max_vocab_size)
+    max_seq_length = 200 # max sequence dimension
+    max_vocab_size = -1 # max vocabulary dimension (-1 for total dimension) (20000)
+    min_count = 5 # discard word with low frequency
+    run(max_seq_length, max_vocab_size, min_count)
+
 
 if __name__ == '__main__':
-	main()
+    main()
