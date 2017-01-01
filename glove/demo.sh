d6090145ed681139057dc604620c6cb6ae24a0b3
Cosimo Casini <casini.cosimo@gmail.com>
2017-01-01 15:32:32 +0100

    clean code and folders. Added evaluationa and graph to script.

--- glove/demo.sh
+++ glove/demo.sh
@@ -6,7 +6,7 @@ set -e
 
 make
 
-DATADIR=data
+DATADIR=../data
 OUTDIR=out
 CORPUS=$DATADIR/train-unsup
 VOCAB_FILE=$OUTDIR/vocab.txt
@@ -18,26 +18,38 @@ VERBOSE=2
 MEMORY=4.0
 VOCAB_MIN_COUNT=5
 VECTOR_SIZE=200
-MAX_ITER=50
+MAX_ITER=1
 WINDOW_SIZE=10
 BINARY=2
 NUM_THREADS=8
 X_MAX=100
+EVALUATE=true
+GRAPH=true
 
+#Create Vocabulary
 echo "$ $BUILDDIR/vocab_count -min-count $VOCAB_MIN_COUNT -verbose $VERBOSE < $CORPUS > $VOCAB_FILE"
 $BUILDDIR/vocab_count -min-count $VOCAB_MIN_COUNT -verbose $VERBOSE < $CORPUS > $VOCAB_FILE
+
+#Create Co-occurrence Matrix
 echo "$ $BUILDDIR/cooccur -memory $MEMORY -vocab-file $VOCAB_FILE -verbose $VERBOSE -window-size $WINDOW_SIZE < $CORPUS > $COOCCURRENCE_FILE"
 $BUILDDIR/cooccur -memory $MEMORY -vocab-file $VOCAB_FILE -verbose $VERBOSE -window-size $WINDOW_SIZE < $CORPUS > $COOCCURRENCE_FILE
+
+#Shuffle Co-occurrence Matrix
 echo "$ $BUILDDIR/shuffle -memory $MEMORY -verbose $VERBOSE < $COOCCURRENCE_FILE > $COOCCURRENCE_SHUF_FILE"
 $BUILDDIR/shuffle -memory $MEMORY -verbose $VERBOSE < $COOCCURRENCE_FILE > $COOCCURRENCE_SHUF_FILE
+
+#Run Glove Model
 echo "$ $BUILDDIR/glove -save-file $SAVE_FILE -threads $NUM_THREADS -input-file $COOCCURRENCE_SHUF_FILE -x-max $X_MAX -iter $MAX_ITER -vector-size $VECTOR_SIZE -binary $BINARY -vocab-file $VOCAB_FILE -verbose $VERBOSE"
 $BUILDDIR/glove -save-file $SAVE_FILE -threads $NUM_THREADS -input-file $COOCCURRENCE_SHUF_FILE -x-max $X_MAX -iter $MAX_ITER -vector-size $VECTOR_SIZE -binary $BINARY -vocab-file $VOCAB_FILE -verbose $VERBOSE
-if [ "$1" = 'matlab' ]; then
-   matlab -nodisplay -nodesktop -nojvm -nosplash < ./eval/matlab/read_and_evaluate.m 1>&2 
-elif [ "$1" = 'octave' ]; then
-   octave < ./eval/octave/read_and_evaluate_octave.m 1>&2
-else
-   echo "$ python eval/python/evaluate.py"
-   python eval/python/evaluate.py
-fi
 
+if $EVALUATE
+  then
+    echo ""
+    python3 eval/python/evaluate.py
+
+    if $GRAPH
+      then
+        echo "I'm building the graph of embeddings..."
+        python3 eval/python/graph.py
+    fi
+fi
