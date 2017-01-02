c25e5b66557820f1d741b98927898573e44b8eb4
Cosimo Casini <casini.cosimo@gmail.com>
2017-01-02 19:15:41 +0100

    removed flags and added function new_tokenizer

--- glove/test_preprop/tkprocessing.py
+++ glove/test_preprop/tkprocessing.py
@@ -3,37 +3,36 @@ import nltk
 from nltk.corpus import stopwords
 import re
 from bs4 import BeautifulSoup
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--punct", help="remove punctuation", action="store_true")
-parser.add_argument("--stop", help="remove stopwords", action="store_true")
-args = parser.parse_args()
 
 #dataDir = "../../data/aclImdb/train/unsup"
 dataDir = "input"
 corpus=""
 count=0
-print("Now Processing " + dataDir + "\n")
+print("Now processing folder: " + dataDir + "\n")
+
+def new_tokenizer(review, punct, stop):
+    #Remove HTML
+    review_text = BeautifulSoup(review, "lxml").get_text()
+
+    #Remove punctuation (falcultative)
+    if punct:
+        review_text = re.sub("[^a-zA-Z]"," ", review_text)
+
+    #Standard English Tokenizer
+    tokens = nltk.word_tokenize(review_text.lower())
+
+    #Remove english stopwords (falcultative)
+    if args.stop:
+        stop = set(stopwords.words('english'))
+        tokens = [i for i in tokens if i not in stop]
+
+    return tokens
 
 for f in os.listdir(dataDir):
     with open(os.path.join(dataDir, f), 'r') as review:
-        #Remove HTML
-        review_text = BeautifulSoup(review, "lxml").get_text()
 
-        #Remove punctuation (falcultative)
-        if args.punct:
-            review_text = re.sub("[^a-zA-Z]"," ", review_text)
-
-        #Standard English Tokenizer
-        tokens = nltk.word_tokenize(review_text.lower())
-
-        #Remove english stopwords (falcultative)
-        if args.stop:
-            stop = set(stopwords.words('english'))
-            tokens = [i for i in tokens if i not in stop]
-
-        corpus += " ".join(tokens) + "\n"
+        review_tkn = new_tokenizer(review, punct=False, stop=True)
+        corpus += " ".join(review_tkn) + "\n"
 
         count+=1
         progress = count/len(os.listdir(dataDir))*100
