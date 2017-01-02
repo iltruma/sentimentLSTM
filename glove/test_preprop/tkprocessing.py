2f856aa02825d190ddc9542e7c1b1b910957fc98
Cosimo Casini <casini.cosimo@gmail.com>
2017-01-02 19:23:08 +0100

    small changes and comments

--- glove/test_preprop/tkprocessing.py
+++ glove/test_preprop/tkprocessing.py
@@ -6,32 +6,36 @@ from bs4 import BeautifulSoup
 
 #dataDir = "../../data/aclImdb/train/unsup"
 dataDir = "input"
-corpus=""
-count=0
+corpus = ""
+count = 0
+
 print("Now processing folder: " + dataDir + "\n")
 
 def new_tokenizer(review, punct, stop):
-    #Remove HTML
+    '''CC
+    Tokenize a review:
+        - Remove HTML
+        - Remove Remove punctuation (optional)
+        - Tokenize and lowercase the words
+        - Remove English stopwords (optional)
+    '''
     review_text = BeautifulSoup(review, "lxml").get_text()
 
-    #Remove punctuation (falcultative)
-    if punct:
-        review_text = re.sub("[^a-zA-Z]"," ", review_text)
+    if punct: review_text = re.sub("[^a-zA-Z]"," ", review_text)
 
-    #Standard English Tokenizer
     tokens = nltk.word_tokenize(review_text.lower())
 
-    #Remove english stopwords (falcultative)
-    if args.stop:
+    if stop:
         stop = set(stopwords.words('english'))
         tokens = [i for i in tokens if i not in stop]
 
     return tokens
 
+#Tokenize every file of dataDir and merge them together
 for f in os.listdir(dataDir):
     with open(os.path.join(dataDir, f), 'r') as review:
 
-        review_tkn = new_tokenizer(review, punct=False, stop=True)
+        review_tkn = new_tokenizer(review, punct=True, stop=True)
         corpus += " ".join(review_tkn) + "\n"
 
         count+=1
@@ -42,4 +46,4 @@ for f in os.listdir(dataDir):
 
 with open("output.txt", "w") as text_file:
     text_file.write(corpus)
-    print("\nFinished!")
+    print("\nFinished :D")
