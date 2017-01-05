585b0bbc847a8ac42c3107fbaa04c91f349b9503
Cosimo Casini <casini.cosimo@gmail.com>
2017-01-05 12:45:33 +0100

    added train_glove.py for training glove model

--- glove/glove_python.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-vocab = "out/vocab.txt"
-
-#subprocess.call(["build/cooccur", "-memory 4.0", "-vocab-file " + vocab, "-verbose 2", "-symmetric 1", "-window-size 10", "<../data/corpus.txt>", "out/cooccurrence.bin"])
-subprocess.call("build/cooccur -memory 4.0 -vocab-file " + vocab + " -verbose 2 -symmetric 1 -window-size 10 <../data/corpus.txt > out/cooccurrence.bin", shell=True)
